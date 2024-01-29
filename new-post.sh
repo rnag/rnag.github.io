2435ca482d5ea94df323f37d4ef5bf4ef0e613da
Ritvik Nag <nagrim7@gmail.com>
2024-01-28 23:08:19 -0500

    Add new tech tips post

--- new-post.sh
+++ new-post.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script generates a new blog post.
 #
@@ -7,21 +7,53 @@
 # Example usage:
 # ./new-post.sh My first post
 
+POSITIONAL_ARGS=()
+CATEGORY=blog
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -t|--tech-tips)
+      CATEGORY=tech-tips
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 DATE=$(date +"%Y-%m-%d")
 FULL_DATE=$(date +"%Y-%m-%d %H:%M:%S %z")
 
 TITLE="$@"
-FILE_TITLE=$(printf "$TITLE" | tr " " "-" | tr "[A-Z]" "[a-z]" | sed s/\?//g)
-FILE_NAME="$DATE-$FILE_TITLE.md"
+FILE_TITLE=$(echo "$TITLE" | tr " " "-" | tr -d "():;?" | tr "[A-Z]" "[a-z]")
+FILE_NAME="$DATE-${FILE_TITLE}.md"
 
 FULL_PATH="_posts/$FILE_NAME"
 
-cat > $FULL_PATH <<EOF
+if [ $CATEGORY == "tech-tips" ]; then
+  TITLE="Tech Tips: ${TITLE}"
+fi
+
+# The first substitution escapes backslashes, the second
+# escapes double-quotes. They must be done in this order.
+# Credits: https://www.reddit.com/r/linuxquestions/comments/thdjgh/comment/i176z2y
+TITLE="${TITLE//\\/\\\\}"
+TITLE="${TITLE//\"/\\\"}"
+
+cat > "$FULL_PATH" <<EOF
 ---
-title: $TITLE
-date: "$FULL_DATE"
+title: "${TITLE}"
+date: "${FULL_DATE}"
 categories:
-  - blog
+  - ${CATEGORY}
 tags:
   - note
   - TODO
